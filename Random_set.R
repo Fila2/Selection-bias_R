2bac23d83234d2fd5828a42523895179bd8c2e65
Fila2 <jal@getica.org>
2016-02-29 23:14:40 +0100

    Added files via upload
    Random Data/class matrix

---
+++ Random_set.R
@@ -0,0 +1,16 @@
+##################################################
+#    Data matrix generation ('random.matrix')    #
+##################################################
+
+## Random generation of gene-associated values
+## - for n_subj (number of subjects)
+## - for n_genes (number of genes)
+## Random allocation of 2 classes ('Normal', 'Disease)
+## Matrix orientation: columns - subjects
+
+random.set <- function(n_subj, n_genes){
+    random.genes <- matrix(rnorm(n_subj*n_genes), ncol = n_subj)                        #Creamos una matriz con numeros sacado al azar de una distribucion normal (rnorm)
+    class <- as.factor(sample(c('Normal', 'Disease'), n_subj, replace = TRUE))
+    random.matrix <- rbind(class, random.genes)
+    return(random.matrix)
+}
